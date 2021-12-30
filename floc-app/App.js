df14726f9440ce6ac47dc8a0f83f57d31deed689
deepak-2309 <deepak.aitpune@gmail.com>
2021-12-30 22:55:17 +0000

    Replaced Stack navigation with Tab navigation.

--- floc-app/App.js
+++ floc-app/App.js
@@ -1,25 +1,49 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CreateEvent from './components/CreateEvent';
 import Events from './components/Events';
+import Profile from './components/Profile';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator
+        // Styling for tab icons. 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Events') {
+              iconName = 'ios-basketball'
+            } else if (route.name === 'CreateEvent') {
+              iconName = 'md-add-circle'
+            } else if (route.name === 'Profile') {
+              iconName = 'md-person';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen
           name="Events"
           component={Events}
-          options={{ title: 'Welcome' }}
+          options={{ title: 'Events' }}
         />
-        <Stack.Screen
+        <Tab.Screen
           name="CreateEvent"
           component={CreateEvent}
-          options={{ title: 'You can create an event here' }}
+          options={{ title: 'Create Event' }}
         />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{ title: 'Profile' }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ floc-app/components/Profile.js
@@ -0,0 +1,11 @@
+import { View, TextInput } from "react-native";
+
+function Profile(){
+    return(
+        <View>
+            <TextInput>This is the user's profile.</TextInput>
+        </View>
+    )
+}
+
+export default Profile;
